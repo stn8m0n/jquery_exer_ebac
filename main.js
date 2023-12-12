11dd47f11bbd7eea15fdb155870d3f411d543708
Maicon Santana <msn-maicon@hotmail.com>
2023-12-12 03:50:51 -0300

    LISTA01

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $('form button').click(function(){
-        $('body h2').slideDown();
-    })
-})
+   $("#send-button").click('submit'function(){
+       $('#list').slideDown();
+    });
+    $("form").on("submit", function (e) {
+        e.preventDefault();
+        const novaTarefa = $("#novo").val(); // Corrigido para "novaTarefa"
+        const novoItem = $(`<ol style="display:none">${novaTarefa}</ol>`); // Corrigido para `<li>`
+        $("#lista-tarefas").append(novoItem); // Adicionando o novo item à lista
+        $(novoItem).fadeIn();
+      });
+});
